b674c5fe9fdef8143caafc4bac820f17551140c0
Moon-DaeSeung <tmdeoans@snu.ac.kr>
2024-12-13 17:23:04 +0900

    fix

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -8,7 +8,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://flitter.dev",
+  site: "https://headless-chart.pages.dev",
   output: "server",
   adapter: cloudflare({
 
