ee63da3207362803955c6bb7c1160f0f2197b101
이동욱 <97590636+Lee-Dongwook@users.noreply.github.com>
2024-12-16 09:59:45 +0900

    Revert "Add pie chart (#17)" (#18)

--- packages/headless-chart/src/charts/pie-chart/default/legend.ts
+++
@@ -1,6 +0,0 @@
-import type { PieChartCustom } from "../types";
-import * as Cartesian from "@shared/cartesian/index";
-
-export function Legend(...args: Parameters<PieChartCustom["legend"]>) {
-  return Cartesian.Legend(args[0]);
-}
