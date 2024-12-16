fc686e9feac3ca230a09382a5749b6b40c39d28b
이동욱 <97590636+Lee-Dongwook@users.noreply.github.com>
2024-12-16 09:30:26 +0900

    Revert "Add pie chart (#17)"

--- packages/headless-chart/src/charts/pie-chart/default/title.ts
+++
@@ -1,6 +0,0 @@
-import type { PieChartCustom } from "../types";
-import * as Cartesian from "@shared/cartesian/index";
-
-export function Title(...args: Parameters<PieChartCustom["title"]>) {
-  return Cartesian.Title(args[0]);
-}
